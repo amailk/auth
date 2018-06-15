fa471356ac7e80547bf896f483251368e328e158
Ama Liyanage <liyaniama@gmail.com>
2018-06-15 14:09:17 -0400

    Sign in attempt fail handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   //deal with feedback from user-state
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    //sign my user in with an email and password
+    //returns a promise that handles some amount of asyncrhonous code.
+    //make use of the promise - get notified when request is complete.
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      //if request fails, then we enter this function
+      //to the catch statement. inside here we start going down the
+      //error chain(in notes)
+      .catch(()=>{
+        firease.auth().createUserWithEmailAndPassword(email,password)
+        .catch(() => {
+        //show error message on screen.
+          this.setState({ error: 'Authentication Failed.' });
+        });
+      });
+  }
 
   render() {
     return (
@@ -30,8 +50,14 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          //by default there is no error. if fails. this will be populated
+          //and there will be an error message.
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in
           </Button>
         </CardSection>
@@ -39,5 +65,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize:20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
